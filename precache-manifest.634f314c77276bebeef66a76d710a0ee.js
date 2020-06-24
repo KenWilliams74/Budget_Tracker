f6c71af7acf3a9a744f45b9c13602eea10e58000
Ken Williams <kenwilliams74@gmail.com>
2020-06-24 02:12:45 -0700

    Updates

---
+++ precache-manifest.634f314c77276bebeef66a76d710a0ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23880482505a0f40357350f7807ebdb",
+    "url": "/Budget_Tracker/index.html"
+  },
+  {
+    "revision": "b297de6899401b49f84a",
+    "url": "/Budget_Tracker/static/css/main.9ef6cc6a.chunk.css"
+  },
+  {
+    "revision": "a10597b5c2854a9e2e33",
+    "url": "/Budget_Tracker/static/js/2.9d42ea6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget_Tracker/static/js/2.9d42ea6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b297de6899401b49f84a",
+    "url": "/Budget_Tracker/static/js/main.8c1b1c8a.chunk.js"
+  },
+  {
+    "revision": "38412bb5a6a7b6df93ef",
+    "url": "/Budget_Tracker/static/js/runtime-main.6428efbf.js"
+  }
+]);
